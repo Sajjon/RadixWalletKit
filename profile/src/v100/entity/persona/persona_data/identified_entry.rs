862491ac8c17fea8023b6d457d061e0b8cd4bd99
Viktor Jansson <v.l.jansson@gmail.com>
2023-12-07 21:16:48 +0100

    Fixed typos and impl of Traits

--- profile/src/v100/entity/persona/persona_data/identified_entry.rs
+++ profile/src/v100/entity/persona/persona_data/identified_entry.rs
@@ -1,41 +1,17 @@
-use crate::v100::entity::persona::persona_data::collection_error::CollectionError;
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
 use std::fmt::Display;
 use uuid::Uuid;
+use wallet_kit_common::error::collection_error::CollectionError;
 
-#[derive(Serialize, Deserialize, Clone, Debug, Default, Hash)]
-pub struct IdentifiedEntry<Value> {
+#[derive(Serialize, Deserialize, Clone, Debug, Default, Hash, Ord, Eq, PartialOrd, PartialEq)]
+pub struct IdentifiedEntry<Value: Eq + Ord> {
     id: ID,
     value: Value,
 }
-impl<Value: Eq> Eq for IdentifiedEntry<Value> {}
-impl<Value: Ord + Iterator> Ord for IdentifiedEntry<Value>
-where
-    <Value as Iterator>::Item: Ord,
-{
-    fn cmp(&self, other: &Self) -> Ordering {
-        (&self.value, &self.id).cmp(&(&other.value, &other.id))
-    }
-}
-
-impl<Value: PartialOrd + Iterator + Ord> PartialOrd for IdentifiedEntry<Value>
-where
-    <Value as Iterator>::Item: Ord,
-{
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.id.cmp(&other.id))
-    }
-}
-
-impl<Value> PartialEq for IdentifiedEntry<Value> {
-    fn eq(&self, other: &Self) -> bool {
-        (&self.id) == (&other.id)
-    }
-}
 
 type ID = Uuid;
-impl<Value: Display> IdentifiedEntry<Value> {
+impl<Value: Display + std::cmp::Ord> IdentifiedEntry<Value> {
     pub fn new(id: ID, value: Value) -> Self {
         Self { id, value }
     }
@@ -51,8 +27,8 @@ impl<Value: Display> IdentifiedEntry<Value> {
     }
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Default, Hash)]
-pub struct CollectionOfIdentifiedEntries<Value> {
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Default, Hash, Ord, PartialOrd)]
+pub struct CollectionOfIdentifiedEntries<Value: Ord> {
     collection: Vec<IdentifiedEntry<Value>>,
 }
 
@@ -74,23 +50,11 @@ impl<Value: Default + std::cmp::Eq + std::cmp::Ord + Copy> CollectionOfIdentifie
     }
 
     pub fn add(&mut self, field: IdentifiedEntry<Value>) -> Result<(), CollectionError> {
-        if self
-            .collection
-            .iter()
-            .map(|item| item.value)
-            .collect::<Vec<Value>>()
-            .contains(&field.value)
-        {
+        if self.collection.iter().any(|i| i.value == field.value) {
             return Err(CollectionError::DuplicateValuesFound);
         }
 
-        if self
-            .collection
-            .iter()
-            .map(|item| item.id)
-            .collect::<Vec<Uuid>>()
-            .contains(&field.id)
-        {
+        if self.collection.iter().any(|i| i.id == field.id) {
             return Err(CollectionError::DuplicateIDOfValuesFound);
         }
 
@@ -100,13 +64,7 @@ impl<Value: Default + std::cmp::Eq + std::cmp::Ord + Copy> CollectionOfIdentifie
     }
 
     pub fn update(&mut self, updated: IdentifiedEntry<Value>) -> Result<(), CollectionError> {
-        if !self
-            .collection
-            .iter()
-            .map(|item| item.id)
-            .collect::<Vec<Uuid>>()
-            .contains(&updated.id)
-        {
+        if !self.collection.iter().any(|i| i.id == updated.id) {
             return Err(CollectionError::PersonaFieldCollectionValueWithIDNotFound);
         }
         let index = self
--- profile/src/v100/entity/persona/persona_data/mod.rs
+++ profile/src/v100/entity/persona/persona_data/mod.rs
@@ -1,3 +1,2 @@
-pub mod collection_error;
 pub mod identified_entry;
 pub mod persona_data;
