cb6efb751f3451640874c03340aa9965004835db
Viktor Jansson <v.l.jansson@gmail.com>
2023-12-13 22:29:23 +0100

    Rewrite embed()

--- profile/src/v100/entity/persona/persona_data/entry_kinds/company_name.rs
+++ profile/src/v100/entity/persona/persona_data/entry_kinds/company_name.rs
@@ -1,11 +1,13 @@
-use crate::v100::entity::persona::persona_data::entry::BasePersonaDataEntry;
+use crate::v100::entity::persona::persona_data::entry::{BasePersonaDataEntry, Entry};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Default)]
-pub struct CompanyName {}
+pub struct CompanyName {
+    name: String,
+}
 
 impl BasePersonaDataEntry for CompanyName {
-    fn embed(&self) -> Self {
+    fn embed(&self) -> Entry {
         self.embed()
     }
 
--- profile/src/v100/entity/persona/persona_data/entry_kinds/name.rs
+++ profile/src/v100/entity/persona/persona_data/entry_kinds/name.rs
@@ -1,11 +1,11 @@
-use crate::v100::entity::persona::persona_data::entry::BasePersonaDataEntry;
+use crate::v100::entity::persona::persona_data::entry::{BasePersonaDataEntry, Entry};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Default)]
 pub struct Name {}
 
 impl BasePersonaDataEntry for Name {
-    fn embed(&self) -> Self {
+    fn embed(&self) -> Entry {
         self.embed()
     }
 
